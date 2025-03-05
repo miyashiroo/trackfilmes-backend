d3201493e8a60ddb95cad04b25d2000988b3445e
Rodrigo Miyashiro <miyashiroo@gmail.com>
2025-03-05 19:16:38 -0400

    cors atualização

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ dotenv.config();
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
